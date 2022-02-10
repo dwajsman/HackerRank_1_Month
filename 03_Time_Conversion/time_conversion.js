42a12dc0da26365e3248d4b3c30441d27989564b
Daniel Wajsman <danielwajsman@mac.com>
2022-02-10 15:13:42 -0800

    final version - passes tests

--- 03_Time_Conversion/time_conversion.js
+++ 03_Time_Conversion/time_conversion.js
@@ -31,7 +31,29 @@ function readLine() {
 
 function timeConversion(s) {
     // Write your code here
-
+    // console.log("s: ",s)
+    // console.log("AM/PM: ", s.substr(s.length - 2))
+    // console.log("hour: ", s.substr(0,2))
+    // console.log(":minute:seconds ", s.substr(2,6))
+    
+    const minuteSeconds = s.substr(2,6)
+    const ampm = s.substr(s.length - 2)
+    let hour = Number(s.substr(0,2))
+    
+    if (ampm === "AM"){
+        if (hour === 12){
+             return ( "00" + minuteSeconds) 
+        } else {
+            if (hour < 10) {return ("0" + hour + minuteSeconds) }
+            return (hour + minuteSeconds) 
+        }    
+    } else {
+        if (hour === 12){
+            return ( hour + minuteSeconds) 
+        } else {
+            return (String(hour+12) + minuteSeconds) 
+        }
+    }
 }
 
 function main() {
